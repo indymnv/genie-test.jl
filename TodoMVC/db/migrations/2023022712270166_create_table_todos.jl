86d68c39d1278b50d913a0bc9dd1f80c58075f52
indymnv <indy.mnv@gmail.com>
2023-02-28 17:57:02 +0900

    new model database

--- TodoMVC/db/migrations/2023022712270166_create_table_todos.jl
+++ TodoMVC/db/migrations/2023022712270166_create_table_todos.jl
@@ -6,15 +6,14 @@ function up()
   create_table(:todos) do
     [
       pk()
-      column(:column_name, :column_type)
-      columns([
-        :column_name => :column_type
-      ])
+      column(:todo, :string)
+      column(:completed, :bool; default = false)
+
     ]
   end
-
-  add_index(:todos, :column_name)
-  add_indices(:todos, :column_name_1, :column_name_2)
+  
+  add_index(:todos, :completed)
+  #add_indices(:todos, :column_name_1, :column_name_2)
 end
 
 function down()
