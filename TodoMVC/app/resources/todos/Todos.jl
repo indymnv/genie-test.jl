7ea9cd67302a4de96a939cb3d52730d52dbf32f1
indymnv <indy.mnv@gmail.com>
2023-03-02 22:08:47 +0900

    add validator

--- TodoMVC/app/resources/todos/Todos.jl
+++ TodoMVC/app/resources/todos/Todos.jl
@@ -3,6 +3,15 @@ module Todos
 import SearchLight: AbstractModel, DbId
 import Base: @kwdef
 
+using SearchLight
+using TodoMVC.TodosValidator
+import SearchLight.Validation: ModelValidator, ValidationRule
+
+SearchLight.Validation.validator(::Type{Todo}) = ModelValidator([
+  ValidationRule(:todo, TodosValidator.not_empty)
+])
+
+
 export Todo
 
 @kwdef mutable struct Todo <: AbstractModel
