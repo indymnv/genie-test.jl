feb1f3dae0ebb2c2666c06e751fdff775c1570d4
indymnv <indy.mnv@gmail.com>
2023-03-07 23:05:42 +0900

    todos_test fix test

--- TodoMVC/test/todos_test.jl
+++ TodoMVC/test/todos_test.jl
@@ -1,8 +1,26 @@
 using Test, SearchLight, Main.UserApp, Main.UserApp.Todos
+using SearchLight.Validation
 
 @testset "Todo unit tests" begin
-
-  ### Your tests here
-  @test 1 == 1
-
+  t = Todo()
+  @testset "Todo is correctly initialized" begin
+    @test t.todo == ""
+    @test t.completed == false
+  end
+  @testset "Todo validates correctly" begin
+     @testset "Todo is invalid" begin
+      v = validate(t)
+      @test haserrors(v) == true
+      @test haserrorsfor(v, :todo) == true
+      @test errorsfor(v, :todo)[1].error_type == :not_empty
+    end
+     @testset "Todo is valid" begin
+      t.todo = "Buy milk"
+      v = validate(t)
+      @test haserrors(v) == false
+      @test haserrorsfor(v, :todo) == false
+      @test errorsfor(v, :todo) |> isempty == true
+    end
+   end
 end;
+
