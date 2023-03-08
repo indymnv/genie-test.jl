571e92914d2ad03d55de3fd9f38df154657323f0
indymnv <indy.mnv@gmail.com>
2023-03-08 22:01:11 +0900

    logic for apis

--- TodoMVC/app/resources/todos/TodosController.jl
+++ TodoMVC/app/resources/todos/TodosController.jl
@@ -10,19 +10,32 @@ using SearchLight.Validation
 
 using TodoMVC.ViewHelper
 
-function index()
+function count_todos()
   notdonetodos = count(Todo, completed = false)
   donetodos = count(Todo, completed = true)
-  alltodos = notdonetodos + donetodos
+  (
+    notdonetodos = notdonetodos,
+    donetodos = donetodos,
+    alltodos = notdonetodos + donetodos
+  )
+end
+function todos()
   todos = if params(:filter, "") == "done"
-    find(Todo, completed = true)
-  elseif params(:filter, "") == "notdone"
-    find(Todo, completed = false)
-  else
-    all(Todo)
-  end
-  html(:todos, :index; todos, notdonetodos, donetodos,
-    alltodos, ViewHelper.active)
+            find(Todo, completed = true)
+          elseif params(:filter, "") == "notdone"
+            find(Todo, completed = false)
+          else
+            # this line was: all(Todo) and is changed to:
+            all(Todo; limit = params(:limit,
+                  SearchLight.SQLLimit_ALL) |> SQLLimit,
+                  offset = (parse(Int, params(:page, "1"))-1) *
+                            parse(Int, params(:limit, "0")))
+          end
+end
+
+function index()
+  html(:todos, :index; todos = todos(), count_todos()...,
+        ViewHelper.active)
 end
 
 
@@ -71,5 +84,107 @@ function delete()
   SearchLight.delete(todo)
   json(Dict(:id => (:value => params(:id))))
 end
+
+### API
+module API
+module V1
+using TodoMVC.Todos
+using Genie.Router
+using Genie.Renderers.Json
+using ....TodosController
+
+using Genie.Requests
+using SearchLight.Validation
+using SearchLight
+
+function list()
+  #all(Todo) |> json
+  TodosController.todos() |> json
+end
+
+function check_payload(payload = Requests.jsonpayload())
+  isnothing(payload) && throw(JSONException(status =
+   BAD_REQUEST, message = "Invalid JSON message received"))
+  payload
+end
+
+function item()
+  todo = findone(Todo, id = params(:id))
+  if todo === nothing
+    return JSONException(status = NOT_FOUND,
+      message = "Todo not found") |> json
+  end
+  todo |> json
+end
+
+function create()
+  payload = try
+    check_payload()
+  catch ex
+    return json(ex)
+  end
+    todo = Todo(todo = get(payload, "todo", ""),
+    completed = get(payload, "completed", false))
+    persist(todo)
+end
+
+function persist(todo)
+  validator = validate(todo)
+  if haserrors(validator)
+    return JSONException(status = BAD_REQUEST,
+      message = errors_to_string(validator)) |> json
+  end
+ try
+    if ispersisted(todo)
+      save!(todo)
+      json(todo, status = OK)
+    else
+      save!(todo)
+      json(todo, status = CREATED, headers =
+        Dict("Location" => "/api/v1/todos/$(todo.id)"))
+    end
+  catch ex
+    JSONException(status = INTERNAL_ERROR,
+            message = string(ex)) |> json
+  end
+
+
+end
+
+function update()
+  payload = try
+    check_payload()
+  catch ex
+    return json(ex)
+  end
+  todo = findone(Todo, id = params(:id))
+  if todo === nothing
+    return JSONException(status = NOT_FOUND,
+      message = "Todo not found") |> json
+  end
+  todo.todo = get(payload, "todo", todo.todo)
+  todo.completed = get(payload, "completed",
+    todo.completed)
+  persist(todo)
+end
+
+function delete()
+  todo = findone(Todo, id = params(:id))
+  if todo === nothing
+    return JSONException(status = NOT_FOUND,
+      message = "Todo not found") |> json
+  end
+  try
+    SearchLight.delete(todo) |> json
+  catch ex
+    JSONException(status = INTERNAL_ERROR,
+      message = string(ex)) |> json
+  end
+end
+
+
+end #v1
+end #api
+
 
 end
